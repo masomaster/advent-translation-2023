5d84032e25744f557c0b9d526e69868b71d55157
masomaster <masonlancaster@protonmail.com>
2023-11-10 19:48:06 -0800

    more testing for deployment

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -24,10 +24,13 @@ async function login(req, res) {
     const user = await User.findOne({
       email: req.body.email,
     });
+    console.log("user", user);
     if (!user) throw new Error();
     const match = await bcrypt.compare(req.body.password, user.password);
+    console.log("match", match);
     if (!match) throw new Error();
     const token = createJWT(user);
+    console.log("token", token);
     res.json(token);
   } catch {
     res.status(400).json("Bad Credentials");
--- server.js
+++ server.js
@@ -41,13 +41,13 @@ app.use("/api/translations", require("./routes/api/translations"));
 
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
   console.log(`Express app running on port ${port}`);

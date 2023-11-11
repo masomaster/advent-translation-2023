942aa6f20671b37876b6e2895bf203d1a3db08e8
masomaster <masonlancaster@protonmail.com>
2023-11-10 20:02:47 -0800

    another cors solution attempt

--- server.js
+++ server.js
@@ -35,19 +35,29 @@ app.use(express.static(path.join(__dirname, "build")));
 
 app.use(require("./config/checkToken"));
 
+// Added to allow CORS access
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Put API routes here, before the "catch all" route
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/translations", require("./routes/api/translations"));
 
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
-// app.get("/*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, function () {
   console.log(`Express app running on port ${port}`);

5b8fce2c1e08420aed3af2a2da86ac1dae5bbe3c
masomaster <masonlancaster@protonmail.com>
2023-11-21 15:18:58 -0800

    updates BASE URL

--- src/utilities/translations-api.js
+++ src/utilities/translations-api.js
@@ -1,6 +1,6 @@
 import sendRequest from "./send-request";
 // This is the base path of the Express route we'll define
-const BASE_URL = "/api/translations";
+const BASE_URL = "https://www.adventtranslation.com/api/translations";
 
 export async function createTranslations(dayTranslations) {
   return sendRequest(BASE_URL, "POST", dayTranslations);

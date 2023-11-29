0ac6b0c276c40a9e963375f6fa4fa7d3225c8519
masomaster <masonlancaster@protonmail.com>
2023-11-29 11:40:08 -0800

    adds retry functionality when connecting to mongodb

--- config/database.js
+++ config/database.js
@@ -1,13 +1,36 @@
+// config/database.js
+
 const mongoose = require("mongoose");
 
-try {
-  mongoose.connect(process.env.DATABASE_URL);
+// Implement a retry mechanism for establishing the initial database connection
+const MAX_RETRIES = 3;
+const RETRY_DELAY_MS = 1000; // 1 second
 
-  const db = mongoose.connection;
+async function connectWithRetries() {
+  let retries = 0;
 
-  db.on("connected", function () {
-    console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
-  });
-} catch (error) {
-  console.error("Error connecting to the database:", error);
+  while (retries < MAX_RETRIES) {
+    try {
+      await mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      const db = mongoose.connection;
+
+      db.on("connected", function () {
+        console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
+      });
+
+      return; // Successfully connected, exit the loop
+    } catch (error) {
+      console.error("Error connecting to the database:", error);
+      retries++;
+      await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY_MS));
+    }
+  }
+
+  throw new Error("Exceeded maximum number of retries for database connection");
 }
+
+connectWithRetries();
